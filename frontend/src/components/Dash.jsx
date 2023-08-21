e8b6a6d75b4d335c37e7dc817afa8ab2828c3065
PESAMS <pesams015@gmail.com>
2023-08-21 17:51:04 +0100

    added a build to the frontend

--- frontend/src/components/Dash.jsx
+++ frontend/src/components/Dash.jsx
@@ -7,24 +7,11 @@ import { useSelector } from 'react-redux';
 const Dash = () => {
   const { isFetching, user, fetchingFailed } = useSelector((state)=> state.user)
 
-  const [timeOfDay, setTimeOfDay] = useState('');
-
-useEffect(() => {
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
-
-  if (currentHour >= 5 && currentHour < 12) {
-    setTimeOfDay('Morning');
-  } else if (currentHour >= 12 && currentHour < 18) {
-    setTimeOfDay('Afternoon');
-  } else {
-    setTimeOfDay('Evening');
-  }
-}, []);
+ 
   return (
     <div>
       <h1 className=" text-end text-3xl text-gray-700 font-semibold">
-      Good {timeOfDay}, {user.userName}!
+
       </h1>
     </div>
   )
